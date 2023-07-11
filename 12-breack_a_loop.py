b727bb080e28776953752dcd088d291b4e150661
poulette <poletserrano59@gmail.com>
2023-07-11 12:19:01 -0400

    actualizado

---
+++ 12-breack_a_loop.py
@@ -0,0 +1,15 @@
+
+
+for letter in "murcielago":
+    print(letter)
+    if letter == "a":
+        break
+
+print("Esto es despues del loop")
+
+print("---------------------")
+
+for letter in "murcielago":
+    if letter == "a":
+        break
+    print(letter)
